be376181bef013fa917dba512d8d409823517878
sadracae <sadrac.aramburo@gmail.com>
2024-09-15 03:38:22 -0600

    Routes tampering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,24 +7,22 @@ import Scene from './models/Scene';
 import Phone from "./components/Phone.jsx"
 import Text from './components/Text.jsx'
 import Questions from './components/Questions.jsx'
+import Landing from './Landing.jsx';
 
 function App() {
   return (
-    <Auth0Provider
+      <div className="">
+      <Auth0Provider
       domain="dev-dfd7qvhsfiu2nhid.us.auth0.com"
       clientId="XGIE1XTU8T8dizhG2i5BXpp1UJfJTgt6"
       authorizationParams={{
         redirect_uri: window.location.origin
       }}
     >
-      <LoginButton />
-      <LogoutButton/>
-      <Profile/>
-      <Scene/>
-      <Questions />
-      <Text />
-      <Phone />
+      <Landing />
     </Auth0Provider>
+
+      </div>
   );
 }
 
---
+++ frontend/src/components/dashboard.jsx
@@ -0,0 +1,11 @@
+const Dashboard = () => {
+    return (
+      <div>
+        <h1>Bienvenido al Dashboard</h1>
+        {/* Aquí puedes agregar más lógica o componentes */}
+      </div>
+    );
+  };
+  
+  export default Dashboard;
+  
